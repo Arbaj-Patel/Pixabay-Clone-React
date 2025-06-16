46792ccc3d231ff7932a72638a7715deda59c2e4
Arbaj Patel <arbajpatel914@gmail.com>
2025-06-16 11:13:34 +0530

    Pixabay Clone Using React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Images from './components/Images.jsx'
+import Navbar from './components/Navbar.jsx'
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Images/>
+    </div>
+  )
+}
+
+export default App
